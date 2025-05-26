a43829318bd9b390bff7eea86100d6cdc0c7e58c
ledsonJyan <jyancastrojl@gmail.com>
2025-05-26 11:37:51 -0300

    mudanças

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,3 +1,20 @@
+let numero = ''
+let operador = ''
 function insert(num){
-    document.getElementById('tela').innerHTML = num
+    if(numero==''){
+        numero = num
+        document.getElementById('tela').innerHTML = numero 
+
+    }else if(num=='C'){
+        numero = ''
+        document.getElementById('tela').innerHTML = numero 
+    }else if(numero==''){
+        numero+=numero
+        document.getElementById('tela').innerHTML = numero 
+    }
+    else{
+        numero = numero + num
+        document.getElementById('tela').innerHTML = numero 
+    }
+    console.log(numero)
 }
