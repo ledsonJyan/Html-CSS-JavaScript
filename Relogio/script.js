91766878401554f62254fd1e8de3984b7a5cc486
ledsonJyan <jyancastrojl@gmail.com>
2025-05-30 06:00:46 -0300

    Relogio

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,3 +1,24 @@
 let hora = document.getElementById('hora');
 let minuto = document.getElementById('minuto');
 let segundo = document.getElementById('segundo');
+
+let relogio = setInterval(function time(){
+    let date_today = new Date();
+    let h = date_today.getHours();
+    let m = date_today.getMinutes();
+    let s = date_today.getSeconds();
+
+    if(h < 10){
+        h = '0' + h
+    }
+    if(m < 10){
+        m = '0' + m
+    }
+    if(s < 10){
+        s = '0' + s
+    }
+
+   hora.textContent = h;
+   minuto.textContent = m;
+   segundo.textContent = s;
+})
